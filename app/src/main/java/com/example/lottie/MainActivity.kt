1834a2b71b1cd75123690a8d3d4870fe52d52427
Yosselin Flores <yoshy.floresg17@gmail.com>
2022-04-23 22:24:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lottie/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lottie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airbnb.lottie.LottieDrawable
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupAnim()
+
+
+    }
+
+
+    private fun setupAnim() {
+        animation_view.setAnimation(R.raw.osito)
+        animation_view.repeatCount = LottieDrawable.INFINITE
+       
+
+        animation_view.playAnimation()
+    }
+}
